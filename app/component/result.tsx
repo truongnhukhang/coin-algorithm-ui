193aec7126a44b5d2ec375e4ac45550b9ae5ab2d
khang <truongnhukhang1993@gmail.com>
2024-02-23 23:32:14 +0700

    update trade statistic

--- app/component/result.tsx
+++ app/component/result.tsx
@@ -117,8 +117,45 @@ export default function BackTestResult(backTestResponse: BackTestResponse) {
                 <div className='font-bold text-gray-900 text-xl  px-8 pt-6 pb-8 mb-4'>
                     <p className=''>Trade statistic</p>
                 </div>
-                <div id="streak-stats" className="flex">
-
+                <div id="streak-stats" className="flex flex-col px-8 ">
+                    <table className="table-auto w-1/2 text-left">
+                        <tr>
+                            <th>Profit - Fee = PNL</th>
+                            <td>{backTestResponse.pnl.toFixed(2)} - {backTestResponse.fee.toFixed(2)} = {backTestResponse.total.toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <th>win trades</th>
+                            <td>{backTestResponse.numWin}</td>
+                        </tr>
+                        <tr>
+                            <th>loss trades</th>
+                            <td>{backTestResponse.numLoose}</td>
+                        </tr>
+                        <tr>
+                            <th>Win streak</th>
+                            <td>{backTestResponse.winStreak?.count} wins , PNL = {backTestResponse.winStreak?.pnl.toFixed(2)}$</td>
+                        </tr>
+                        <tr>
+                            <th>Loss streak</th>
+                            <td>{backTestResponse.looseStreak?.count} lost , PNL = {backTestResponse.looseStreak?.pnl.toFixed(2)}$</td>
+                        </tr>
+                        <tr>
+                            <th>avg win streak</th>
+                            <td>{backTestResponse.avgWinStreak.toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <th>avg loss streak</th>
+                            <td>{backTestResponse.avglooseStreak.toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <th>Max drawdown value</th>
+                            <td>{backTestResponse.drawDownVal?.value.toFixed(2)} - Start from {new Date(Number(backTestResponse.drawDownVal?.startDate)).toLocaleDateString()} to {new Date(Number(backTestResponse.drawDownVal?.endDate)).toLocaleDateString()}</td>
+                        </tr>
+                        <tr>
+                            <th>Max drawdown percent</th>
+                            <td>{backTestResponse.drawdownPer?.value.toFixed(3) * 100}% - Start from {new Date(Number(backTestResponse.drawdownPer?.startDate)).toLocaleDateString()} to {new Date(Number(backTestResponse.drawdownPer?.endDate)).toLocaleDateString()}</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
